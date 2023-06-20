a604594b3cc6bf7cf246f70233d8d16d5ad7c9fb
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-06-20 11:26:50 +0600

    Setup and Test db connections

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import "dotenv/config"
+
+const DB_URL: string = process.env.mongourl!
+
+export default async function testDb() {
+  try {
+    await mongoose.connect(DB_URL)
+    console.log("db connected")
+  } catch(e) {
+    console.log(e)
+  } finally {
+    mongoose.disconnect()
+    console.log("db disconnected")
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import app from "./server"
 import dotenv from "dotenv"
+import testDb from "./db";
 
 dotenv.config()
 
 const PORT: string | number = process.env.PORT || 8080;
+
+testDb()
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} ...`)
