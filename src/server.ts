025215ecdc0119ca2a69a0bc199a626fe6b72804
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-06-21 10:24:36 +0600

    Add bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import bodyParser from "body-parser";
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200)
