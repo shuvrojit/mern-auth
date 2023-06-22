d63d779844f365d5760d266f6834b3cb8bf00c21
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-06-23 05:39:13 +0600

    Add mongodb connection

--- src/db.ts
+++ src/db.ts
@@ -3,14 +3,8 @@ import "dotenv/config";
 
 const DB_URL: string = process.env.mongourl!;
 
-export default async function testDb() {
-  try {
-    await mongoose.connect(DB_URL);
-    console.log("db connected");
-  } catch (e) {
-    console.log(e);
-  } finally {
-    mongoose.disconnect();
-    console.log("db disconnected");
-  }
+const connect = () => {
+  return mongoose.connect(DB_URL)
 }
+
+export default connect
