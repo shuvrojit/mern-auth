bfe5d0ff774dd3655caccdc2df56f3826400bf71
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-06-23 06:14:38 +0600

    Update User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,14 +6,26 @@ interface IUser {
   userName: string;
   email: string;
   password: string;
+  date: Date;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
   firstName: String,
   lastName: String,
-  userName: String,
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: String,
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const User = mongoose.model("user", userSchema);
