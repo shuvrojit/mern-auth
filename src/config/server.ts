c58a8dc97765d8a23bcf052a571e19fa947dacbb
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-29 22:42:56 +0600

    Add logging with morgan and cors

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,10 +3,15 @@ import express, { Express, Request, Response } from "express";
 import { SignUp, LogIn } from "../controllers/user";
 import connectDB from "./db"
 import { protectedRoute, adminRoute } from "../middleware/auth";
+import morgan from "morgan"
+import cors from "cors"
 
 const app: Express = express();
 
 connectDB()
+
+app.use(morgan("combined"))
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
