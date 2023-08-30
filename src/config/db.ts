1185a185f79720dac7036987fed154998bb2bb3c
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-30 15:50:53 +0600

    Change dbname

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ const DB_URL: string = process.env.mongoURL!;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL, {dbName: "simple-auth"});
     process.stdout.write("Database connected\n");
   } catch (e) {
     process.stdout.write(`Error ${e}\n`);
